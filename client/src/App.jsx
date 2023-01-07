7f3d088ed393a0cb6a1c3a2ab6c8a13f2185c4e3
Fariol Blondeau <blondeau.nbif@mail.ru>
2023-01-07 17:52:30 +0300

    removing of useEffect and fetch data using react-query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import ListPost from "./components/ListPost";
-import AddFrom from "./components/AddForm"
-import './App.css';
-
+import AddFrom from "./components/AddForm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
 
 function App() {
+  const queryClient = new QueryClient();
 
   return (
     <div className="App">
-      <AddFrom />
-      <ListPost />
+      <QueryClientProvider client={queryClient}>
+        <AddFrom />
+        <ListPost />
+      </QueryClientProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/ListPost.jsx
+++ client/src/components/ListPost.jsx
@@ -1,26 +1,24 @@
-import { useState, useEffect, Children } from "react";
+import { Children } from "react";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Post from "./Post";
 
 function ListPost() {
-  const [posts, setPosts] = useState([]);
+  const { data: posts, isLoading} = useQuery(["post"], getPosts);
 
-  const handlePosts = async () => {
-    try {
-      const { data } = await axios.get("http://localhost:5000");
-      setPosts(data);
-    } catch (err) {
-      console.log(`Failed to load data error: ${err.message}`);
-    }
-  };
 
-  useEffect(() => {
-    handlePosts();
-  }, []);
+  async function getPosts(){
+    const res = await axios.get("http://localhost:5000");
+    return res.data;
+  }
+
+  if(isLoading){
+    return <div className="loading">Loading...</div>
+  }
 
   return (
     <div className="list__post">
-      {Children.toArray(posts.map((post) => <Post {...post} />))}
+      {Children.toArray(posts?.map((post) => <Post {...post} />))}
     </div>
   );
 }
